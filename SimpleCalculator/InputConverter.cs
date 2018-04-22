2007cd8301864d84187bc1dc60097175e671ab3b
Jude Ocheje <ochejejude@gmail.com>
2018-04-22 09:21:07 +0100

    add method to convert user input to number

---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumber(string input)
+        {
+            double convertedInput;
+            if (!double.TryParse(input, out convertedInput)) throw  new ArgumentException("Expected input should be a number");
+            return convertedInput;
+        }
+    }
+}

e745df164a706f0003f8cd58ec07165b7292ab96
Jude Ocheje <ochejejude@gmail.com>
2018-04-22 09:24:05 +0100

    add method to take care of calculating various inputs

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+        public double Calculate(string operation, double firstNumber, double secondNumber)
+        {
+            double result;
+
+            switch (operation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "subtraction":
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "multiplication":
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "division":
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                default:
+                    throw new InvalidExpressionException("Provided operation is not a known type.");
+            }
+
+            return result;
+        }
+    }
+}
